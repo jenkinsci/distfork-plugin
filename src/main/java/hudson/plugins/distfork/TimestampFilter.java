e8f30a37e1e230916989c7de2a273550e178fdb1
kohsuke <kohsuke@dev.java.net>
2010-01-11 17:59:57 +0000

    this class needs to be serializable

--- src/main/java/hudson/plugins/distfork/TimestampFilter.java
+++ src/main/java/hudson/plugins/distfork/TimestampFilter.java
@@ -2,11 +2,12 @@ package hudson.plugins.distfork;
 
 import java.io.FileFilter;
 import java.io.File;
+import java.io.Serializable;
 
 /**
  * @author Kohsuke Kawaguchi
  */
-final class TimestampFilter implements FileFilter {
+final class TimestampFilter implements FileFilter, Serializable {
     private final long startTime;
 
     public TimestampFilter(long startTime) {
@@ -16,4 +17,6 @@ final class TimestampFilter implements FileFilter {
     public boolean accept(File f) {
         return f.lastModified()>startTime;
     }
+
+    private static final long serialVersionUID = 1L;
 }
